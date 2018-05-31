727ab80d6606837cb79027baf8abeaf0e2961fca
Anthony <anthony@anthonys-mbp.lan>
2018-05-31 11:49:04 -0400

    User Post, getby

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,76 +2,67 @@
 const router = require('express').Router();
 const User = require('../models/userModel');
 
-// // Endpoint (1) Post User
-// router.post('/', function post(req, res) {
-//     const userData = req.body;
-//     const user = new User(userData);
+// Endpoint (1) Post User
+router.post('/', (req, res) => {
+    const userData = req.body;
+    const user = new User(userData);
 
-//     user
-//         .save()
-//         .then(user => {
-//             res.status(201).json(user);
-//         })
-//         .catch(err => {
-//             res.status(500).json(err);
-//         });
-// });
+    user
+        .save()
+        .then(users => {
+            res.status(201).json(users);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
-// // Endpoint (2) Get All Users
-// router.get('/', function (req, res) {
-//     User.find().then(users => {
-//         res.status(200).json(users);
-//     });
-// });
+// Endpoint (2) Get All Users
+router.get('/', (req, res) => {
+    User.find().then(users => {
+        res.status(200).json(users);
+    });
+});
 
-// // Endpoint (3) Get Users by ID
-// router.get('/:id', function get(req, res) {
-//     const { id } = req.params;
+// // Endpoint (3) Get Notes by Id
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
 
-//     User.findById(id)
-//         .then(user => {
-//             res.status(200).json(user);
-//         })
-//         .catch(err => {
-//             res.status(500).json(err);
-//         });
-// });
+    User.findById(id)
+    .then(users => {
+        res.status(202).json(users);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
-// // Endpoint (4) Delete Users by Id
-// router.delete('/:id', (req, res) => {
-//     const { id } = req.params;
+// Endpoint (4) Delete users by Id
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
 
-//     User
-//         .findByIdAndRemove(id)
-//         .then(user => {
-//             res.status(200).json(user);
-//         })
-//         .catch(err => {
-//             res.status(500).json(err);
-//         });
-// });
+    User.findByIdAndRemove(id)
+    .then(users => { 
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(404).json(err);
+    });
+});
 
-// // Endpoint (5) Edit (put) Users by Id
-// // router.put('/users/:id', (req, res) => {
-// //     const { id } = req.params;
-// //     const updatedUser = req.body;
-    
-// //     const options = {
-// //         new: true,
-// //     };
+// Endpoint (5) Edit Users by Id
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+    const options = {
+        new: true,
+    };
 
-// //     User
-// //         .findByIdAndUpdate(id, updatedUser, options)
-// //         .then(user => {
-// //             if(user) {
-// //                 res.status(200).json(user)
-// //             } else {
-// //                 res.status(404).json({ message: 'User not found' });
-// //             }
-// //         })
-// //         .catch(err => {
-// //             res.status(500).json(err);
-// //         });
-// // });
+    User.findByIdAndUpdate(id, updatedUser, options).then(update => {
+        res.status(200).json(update)
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+})
 
 module.exports = router;
